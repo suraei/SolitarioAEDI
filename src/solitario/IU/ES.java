87da9238fb83e310a41ade6b9c28be48e48f4f2e
Suraei <cris1998lm@gmail.com>
2020-04-18 10:30:13 +0200

    Versión 0.3 Correción clase pideNumero :bug:

--- src/solitario/IU/ES.java
+++ src/solitario/IU/ES.java
@@ -1,4 +1,3 @@
-
 package solitario.IU;
 
 import java.util.Scanner;
@@ -7,28 +6,34 @@ import java.util.Scanner;
  *
  * @author AEDI
  */
-public class ES
-{
-    public static Scanner leer = new Scanner(System.in);
-    
-    public static String pideCadena(String mensaje)
-    {
-         // Poner el mensaje
-            System.out.print(mensaje);
-               
-             // Pedir
-            return leer.nextLine();
-               
-    }
-    
-        
-    public static int pideNumero(String mensaje)
-    {
-        
-            System.out.print(mensaje);
+public class ES {
 
-            // Pedir
-            return Integer.parseInt(leer.nextLine());
-       
+    public static Scanner leer = new Scanner(System.in);
+
+    public static String pideCadena(String mensaje) {
+        // Poner el mensaje
+        System.out.print(mensaje);
+
+        // Pedir
+        return leer.nextLine();
+
+    }
+
+    public static int pideNumero(String mensaje) { // Correccion error si no se introduce un int 
+        int numero = -1;
+        boolean error;
+
+        do {
+            error = false;
+            System.out.print(mensaje);
+            try {
+                numero = Integer.parseInt(leer.nextLine());
+            } catch (NumberFormatException err) {
+                error = true;
+            }
+        } while (error);
+
+        return numero;
+
     }
 }
